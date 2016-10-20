1192a340340edd7abf4797d90a9e6ea9b7f36da1
Zack Forbing <zackforbing@gmail.com>
2016-10-19 22:38:38 -0600

    fix jshint errors

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-require('./player_test')
-require('./game_test')
-require('./imageRepository_test')
-require('./enemy_test')
+require('./player_test');
+require('./game_test');
+require('./imageRepository_test');
+require('./enemy_test');
--- test/player_test.js
+++ test/player_test.js
@@ -2,7 +2,7 @@ const chai   = require('chai');
 const assert = chai.assert;
 const Player = require('../lib/player');
 const stub   = require('./support/stub');
-const Game   = require('../lib/game')
+const Game   = require('../lib/game');
 
 describe('Player', function() {
   context('with default attributes', function() {
@@ -18,7 +18,7 @@ describe('Player', function() {
       let player = new Player(game);
 
       assert.instanceOf(player, Player);
-    })
+    });
 
     it('should have an x-coordinate', function() {
       var canvas = stub();
@@ -30,7 +30,7 @@ describe('Player', function() {
       let player = new Player(game);
 
       assert.equal(player.center.x, 400);
-    })
+    });
 
     it('should have an y-coordinate', function() {
       var canvas = stub();
@@ -42,7 +42,7 @@ describe('Player', function() {
       let player = new Player(game);
 
       assert.equal(player.center.y, 400);
-    })
+    });
 
     it('should have a height', function() {
       var canvas = stub();
@@ -54,7 +54,7 @@ describe('Player', function() {
       let player = new Player(game);
 
       assert.equal(player.size.x, 50);
-    })
+    });
 
     it('should have a width', function() {
       var canvas = stub();
@@ -66,6 +66,6 @@ describe('Player', function() {
       let player = new Player(game);
 
       assert.equal(player.size.y, 50);
-    })
+    });
   });
 });
